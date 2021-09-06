731e71320f3cda070b15746020a00f61e9b73735
Stoo <stoo.goff@gmail.com>
2021-09-07 00:47:01 +0100

    Removed package-lock.json. Fixed distribution secret key.

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'weevolve',
+    title: 'we evolve',
     htmlAttrs: {
       lang: 'en'
     },
